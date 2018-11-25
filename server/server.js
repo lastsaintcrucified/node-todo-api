fdf5079b8f2aa1f5bc987629f1d7ac3bd9885712
unknown <lastsaintcrucified@gmail.com>
2018-11-25 08:50:16 +0600

    Add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -28,6 +28,25 @@ app.get('/todos',(req, res) =>{
   }, (e) =>{
     res.status(400).send(e);
   })
+});
+
+// GET /todos/1233121212
+app.get('/todos/:id',(req, res) =>{
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo) =>{
+    if(!todo){
+      return res.status(404).send();
+    }
+    return res.send(todo);
+  }, (e) =>{
+    return console.log(e);
+  });
+
 });
 
 app.listen(3000,() =>{
