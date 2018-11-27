5f7543c1defe56569ff365b4eb1510b112eb6213
unknown <lastsaintcrucified@gmail.com>
2018-11-27 08:42:48 +0600

    Add PATCH /todos/id

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
-
-var {mongoose} = require('./db/mongoose');
+const _ = require('lodash');
+const {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var{User} = require('./models/user')
 var port = process.env.PORT || 3000;
 var app = express();
 
+
 app.use(bodyParser.json());
+
+
 
 app.post('/todos', (req, res) =>{
   var todo = new Todo({
@@ -22,6 +25,8 @@ app.post('/todos', (req, res) =>{
   })
 });
 
+
+
 app.get('/todos',(req, res) =>{
   Todo.find().then((todos) =>{
     res.send({todos});
@@ -29,6 +34,8 @@ app.get('/todos',(req, res) =>{
     res.status(400).send(e);
   })
 });
+
+
 
 // GET /todos/1233121212
 app.get('/todos/:id',(req, res) =>{
@@ -46,8 +53,9 @@ app.get('/todos/:id',(req, res) =>{
   }, (e) =>{
     return console.log(e);
   });
-
 });
+
+
 
 app.delete('/todos/:id',(req, res) =>{
   var id = req.params.id;
@@ -58,11 +66,36 @@ app.delete('/todos/:id',(req, res) =>{
 
   Todo.findByIdAndRemove(id).then((todo) =>{
     if(!todo){
-      return res.status(400).send();
+      return res.status(404).send();
     }
     res.status(200).send({todo});
   }).catch((e) => res.status(400).send(e));
 });
+
+
+
+app.patch('/todos/:id',(req, res) =>{
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text','completed']);
+  if(!ObjectID.isValid(id)){
+    return res.status(400).send();
+  }
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) =>{
+    if(!todo){
+      return res.status(404).send({});
+    }
+    res.send({todo});
+  }).catch((e) => res.status(400).send());
+
+});
+
+
 
 app.listen(port,() =>{
   console.log(`Uploaded on ${port}....`);
